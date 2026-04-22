4126c408d5e5b18aa7b80ba65e89569c85d58e66
maria.mejia <desarrollo@academicos.cesde.edu.co>
2026-04-21 20:45:42 -0500

    feat se crea limpieza

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import pandas as pd
 
 from utils.simulacion import generar_simulacion
-from notebook.limpieza import limpiar_datos
 
-#Llamando a las rutinas de simulacion
+from notebook.limpieza import limpiar_simulacion
+
 simulaciones=generar_simulacion(10)
 
-#Llamando a pandas para crear data frames de los datos de entrada
 simulaciones_ordenadas=pd.DataFrame(simulaciones)
 
-#LLamando a la rutina de limpieza
-simulaciones_limpias=limpiar_datos(simulaciones_ordenadas)
+simulaciones_limpias=limpiar_simulacion(simulaciones_ordenadas)
 print(simulaciones_limpias)
