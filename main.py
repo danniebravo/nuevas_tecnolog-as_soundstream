7f6a19499e23a860fa818793f1ebc51b52be8117
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2026-04-22 08:28:33 -0500

    feat se crea limpieza

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import pandas as pd
 
+#zona para importar simulaciones
 from utils.simulacion import generar_simulacion
 
-from notebook.limpieza import limpiar_simulacion
+#zona para importar limpiezas
+from notebook.limpieza import limpiar_datos
 
+#Creando las simulaciones
 simulaciones=generar_simulacion(10)
 
+#Ordenando las simulaciones
 simulaciones_ordenadas=pd.DataFrame(simulaciones)
 
-simulaciones_limpias=limpiar_simulacion(simulaciones_ordenadas)
+#limpiando el set de datos
+simulaciones_limpias=limpiar_datos(simulaciones_ordenadas)
 print(simulaciones_limpias)
