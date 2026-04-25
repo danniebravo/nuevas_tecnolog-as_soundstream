dbf2b2b63bfd7a6916752d4d3523b5e80c321eea
desarrollo <desarrollo@academicos.cesde.edu.co>
2026-04-25 11:46:35 -0500

    feat se crea rutina descripcion

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from utils.simulacion import generar_simulacion
 #zona para importar limpiezas
 from notebook.limpieza import limpiar_datos
 
+#zona para importar descripciones
+from notebook.descripcion import describir_datos
+
 #Creando las simulaciones
 simulaciones=generar_simulacion(10)
 
@@ -14,4 +17,7 @@ simulaciones_ordenadas=pd.DataFrame(simulaciones)
 
 #limpiando el set de datos
 simulaciones_limpias=limpiar_datos(simulaciones_ordenadas)
-print(simulaciones_limpias)
+
+#describiendo los datos
+describir_datos(simulaciones_limpias)
+
